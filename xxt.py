03486920d22a61d7aa761cf11c1ba650d3ecc97a
极客蓝鱼 <silence2021silence@163.com>
2022-12-18 21:43:52 +0800

    Update xxt.py
    修复pyinstaller打包.exe后运行闪退问题

--- xxt.py
+++ xxt.py
@@ -1,9 +1,9 @@
 # -*- coding=utf-8 -*-
 """
-Time:        2022/12/12 16:58
-Version:     V 0.0.1
+Time:        2022/12/18 21:40
+Version:     V 0.0.2
 File:        xxt.py
-Describe:    
+Describe:
 Author:      Lanyu
 E-Mail:      silence2021silence@163.com
 Gitee link:  https://gitee.com/silence2021silence/
@@ -12,6 +12,7 @@ Github link: https://github.com/silence2021silence/
 
 import time
 import pyautogui
+import os
 
 
 def get_task():
@@ -22,23 +23,26 @@ def get_task():
         j = lines[i + 2].replace("\n", "")
         k = lines[0].replace("\n", "")
         m = lines[1].replace("\n", "")
-        task_list.append(j)
-        task_list.append(k)
-        task_list.append(m)
+        task_list.append("img/" + j)
+        task_list.append("img/" + k)
+        task_list.append("img/" + m)
     return task_list
 
 
 def click(img):
     while True:
+        if not os.path.exists(img):
+            print("找不到" + img + "文件")
+            input("输入任意内容退出\n")
         location = pyautogui.locateCenterOnScreen(img, confidence=0.9)
         if location is not None:
             pyautogui.click(location.x, location.y, interval=0.2, duration=0.5, button="left")
             break
         else:
-            if img == "replay.png":
+            if img == "img/replay.png":
                 print("等待播放完毕")
                 time.sleep(1)
-            elif img == "play.png":
+            elif img == "img/play.png":
                 time.sleep(2)
                 pyautogui.moveTo(1400, 700, duration=0.5)
                 pyautogui.scroll(-200)
@@ -52,7 +56,6 @@ def click(img):
 
 def main():
     task_list = get_task()
-    print(task_list)
     i = 0
     while i < len(task_list):
         img = task_list[i]
@@ -62,4 +65,10 @@ def main():
 
 
 if __name__ == "__main__":
+    if not os.path.exists("img"):
+        print("找不到img文件夹")
+        input("输入任意内容退出\n")
+    if not os.path.exists("task.txt"):
+        print("找不到task.txt文件")
+        input("输入任意内容退出\n")
     main()
